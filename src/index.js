5178a2835e9b304a60fbeaf26fdfc923cbd65020
angelacuahutle <angela.cuahutle@gmail.com>
2021-11-30 15:32:03 -0600

    update index.js and print.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+import printMe from './print.js';
+
 function component() {
+  
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
